0f50d2d6a69ce133ba9f72bb68acaa081f2315b2
Zeyadhatem391 <zeyadhatemsabry@gmail.com>
2026-03-10 21:08:47 +0200

    actors page

--- src/app/(pages)/actors/page.tsx
+++ src/app/(pages)/actors/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+export const dynamic = "force-dynamic";
 import Back from "@/components/common/Back";
 import FilterButton from "../genre/components/FilterButton";
 import SortButton from "../genre/components/SortButton";
