34ac1ed1395bf9a7cc5565a7423f675f49dc474e
Zeyadhatem391 <zeyadhatemsabry@gmail.com>
2026-03-10 21:00:03 +0200

    edite actors page

--- src/app/(pages)/actors/page.tsx
+++ src/app/(pages)/actors/page.tsx
@@ -1,8 +1,33 @@
+import Back from "@/components/common/Back"
+import FilterButton from "../genre/components/FilterButton"
+import SortButton from "../genre/components/SortButton"
+import InputSearch from "@/components/common/InputSearsh"
 
 
 const ActorsPage = () => {
   return (
-    <div>ActorsPage</div>
+      <div className="max-w-6xl mx-auto px-4">
+      <div className="flex items-center gap-3 w-full my-3">
+        <Back />
+        <InputSearch />
+      </div>
+
+      <div className="flex flex-col items-center justify-center gap-4 mt-6">
+        <h2 className="text-3xl font-bold text-center">Actors</h2>
+        <div className="flex items-center gap-3">
+          <SortButton />
+          <FilterButton />
+        </div>
+      </div>
+
+      <div className="my-10">
+        {/* <ActorsCards page={page} /> */}
+      </div>
+
+      <div className="flex items-center justify-center gap-4 mt-6">
+        {/* <PaginationDemo page={page} setPage={changePage} /> */}
+      </div>
+    </div>
   )
 }
 
