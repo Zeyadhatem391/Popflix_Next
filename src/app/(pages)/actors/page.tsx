5c0ebc123ef25626dc3d10053a6348a4cb0583ea
Zeyadhatem391 <zeyadhatemsabry@gmail.com>
2026-03-10 21:04:30 +0200

    edite actors page

--- src/app/(pages)/actors/page.tsx
+++ src/app/(pages)/actors/page.tsx
@@ -1,12 +1,18 @@
-import Back from "@/components/common/Back"
-import FilterButton from "../genre/components/FilterButton"
-import SortButton from "../genre/components/SortButton"
-import InputSearch from "@/components/common/InputSearsh"
-
+"use client";
+import Back from "@/components/common/Back";
+import FilterButton from "../genre/components/FilterButton";
+import SortButton from "../genre/components/SortButton";
+import InputSearch from "@/components/common/InputSearsh";
+import ActorsCards from "./components/ActorsCards";
+import { useSearchParams } from "next/navigation";
 
 const ActorsPage = () => {
+  const searchParams = useSearchParams();
+
+  const page = Number(searchParams.get("page")) || 1;
+
   return (
-      <div className="max-w-6xl mx-auto px-4">
+    <div className="max-w-6xl mx-auto px-4">
       <div className="flex items-center gap-3 w-full my-3">
         <Back />
         <InputSearch />
@@ -21,14 +27,14 @@ const ActorsPage = () => {
       </div>
 
       <div className="my-10">
-        {/* <ActorsCards page={page} /> */}
+        <ActorsCards page={page} />
       </div>
 
       <div className="flex items-center justify-center gap-4 mt-6">
         {/* <PaginationDemo page={page} setPage={changePage} /> */}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ActorsPage
+export default ActorsPage;
