e6f944947c031e7e71902206af3bbe89f115e896
Zeyadhatem391 <zeyadhatemsabry@gmail.com>
2026-03-11 02:29:44 +0200

    edite actors page

---
+++ src/app/(pages)/actors/components/ActorsClient.tsx
@@ -0,0 +1,67 @@
+"use client";
+
+import Back from "@/components/common/Back";
+import InputSearch from "@/components/common/InputSearsh";
+import { useRouter, usePathname } from "next/navigation";
+import { useQueryClient } from "@tanstack/react-query";
+import { useEffect } from "react";
+import ActorsCards from "../components/ActorsCards";
+import { GetActors } from "@/hooks/useGetAllActors";
+import SortButton from "../../genre/components/SortButton";
+import FilterButton from "../../genre/components/FilterButton";
+import { PaginationDemo } from "../../components/PaginationGenre";
+
+type Props = {
+  page: number;
+};
+
+const ActorsClient = ({ page }: Props) => {
+  const router = useRouter();
+  const pathname = usePathname();
+  const queryClient = useQueryClient();
+
+  const changePage = (newPage: number) => {
+    if (newPage < 1 || newPage > 500) return;
+
+    router.push(`${pathname}?page=${newPage}`);
+  };
+
+  useEffect(() => {
+    const nextPage = page + 1;
+
+    if (nextPage > 500) return;
+
+    queryClient.prefetchQuery({
+      queryKey: ["actors", nextPage],
+      queryFn: () => GetActors(nextPage),
+    });
+  }, [page, queryClient]);
+
+  return (
+    <div className="max-w-6xl mx-auto px-4">
+      <div className="flex items-center gap-3 w-full my-3">
+        <Back />
+        <InputSearch />
+      </div>
+
+      <div className="flex flex-col items-center justify-center gap-4 mt-6">
+        <h2 className="text-3xl font-bold text-center">Actors</h2>
+
+        <div className="flex items-center gap-3">
+          <SortButton />
+          <FilterButton />
+        </div>
+      </div>
+
+      <div className="my-10">
+        <ActorsCards page={page} />
+      </div>
+
+      <div className="flex items-center justify-center mt-6">
+        <PaginationDemo page={page} setPage={changePage} />
+      </div>
+    </div>
+  );
+};
+
+export default ActorsClient;
