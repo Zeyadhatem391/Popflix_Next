f706978fffc12ec79876fed9250b9493c8522698
Zeyadhatem391 <zeyadhatemsabry@gmail.com>
2026-06-30 17:06:04 +0300

    add matadata in movies details

--- src/hooks/Movies/useGetDetailsMovies.ts
+++ src/hooks/Movies/useGetDetailsMovies.ts
@@ -1,32 +1,12 @@
-import { Movie } from "@/lib/types/Movie";
+
 import { useQuery } from "@tanstack/react-query";
-
-/* ========= TYPES ========= */
-
-
-
-/* ========= FETCH ========= */
-
-const GetDetailsMovies = async (
-  movieId: string
-): Promise<Movie> => {
-  const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/3/movie/${movieId}?api_key=${process.env.NEXT_PUBLIC_API_KEY}&include_adult=false&append_to_response=credits,videos`
-  );
-
-  if (!response.ok) {
-    throw new Error("Failed to fetch movie");
-  }
-
-  return response.json();
-};
-
-/* ========= HOOK ========= */
+import { Movie } from "@/lib/types/Movie";
+import { getMovieDetails } from "@/services/movies/getMovieDetails";
 
 const useGetDetailsMovies = (movieId: string) => {
   return useQuery<Movie>({
     queryKey: ["movie-details", movieId],
-    queryFn: () => GetDetailsMovies(movieId),
+    queryFn: () => getMovieDetails(movieId),
     staleTime: 1000 * 60 * 5,
     enabled: !!movieId,
   });
