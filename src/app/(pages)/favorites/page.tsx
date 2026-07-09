078d79925915bc5f957f1f6703efdec4c0cc47c3
Zeyadhatem391 <zeyadhatemsabry@gmail.com>
2026-07-09 20:16:17 +0300

    fix: edit favorites

--- src/app/(pages)/favorites/page.tsx
+++ src/app/(pages)/favorites/page.tsx
@@ -1,34 +1,11 @@
-export const dynamic = "force-dynamic";
-import { authOptions } from "@/lib/nextAuth";
-import { getServerSession } from "next-auth";
-import Link from "next/link";
-import FavoritesClient from "../../../modules/favorites/components/FavoritesClient";
+import { Suspense } from "react";
+import FavoritesMoviesSkeleton from "@/shared/components/skeletons/FavoritesMoviesSkeleton";
+import FavoritesPageContent from "@/modules/favorites/components/FavoritesPageContent";
 
-const FavoritesPage = async () => {
-  const session = await getServerSession(authOptions);
-
+export default function Page() {
   return (
-    <section className="w-full min-h-screen flex justify-center px-4 py-5 bg-[#111]">
-      {session ? (
-        <FavoritesClient />
-      ) : (
-        <div className="flex flex-col items-center text-center space-y-6 max-w-md justify-center">
-          <h1 className="text-3xl md:text-4xl font-bold text-white">
-            Welcome to Popfliex
-          </h1>
-          <p className="text-gray-300 text-lg md:text-xl">
-            You must log in to access your Favorites page.
-          </p>
-          <Link
-            href="/login"
-            className="px-6 py-3 bg-yellow-500 text-gray-900 font-semibold rounded-lg hover:bg-yellow-400 transition"
-          >
-            Log In
-          </Link>
-        </div>
-      )}
-    </section>
+    <Suspense fallback={<FavoritesMoviesSkeleton />}>
+      <FavoritesPageContent />
+    </Suspense>
   );
-};
-
-export default FavoritesPage;
+}
