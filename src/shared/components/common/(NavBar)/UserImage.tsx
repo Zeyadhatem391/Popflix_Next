36e0efd8bbc6e7cdd162e793047a303b91298d7a
Zeyadhatem391 <zeyadhatemsabry@gmail.com>
2026-07-12 00:29:30 +0300

    edit error

--- src/shared/components/common/(NavBar)/UserImage.tsx
+++ src/shared/components/common/(NavBar)/UserImage.tsx
@@ -1,26 +1,20 @@
 import Link from "next/link";
 import DropdownProfile from "./DropdownProfile";
-import { auth } from "@/auth";
 import { getProfile } from "@/modules/profile/api/getProfile";
-import { redirect } from "next/navigation";
 
 export default async function UserImage() {
-  const session = await auth();
-
   const profile = await getProfile();
 
   if (!profile) {
-    redirect("/login");
+    return (
+      <Link
+        href="/login"
+        className="relative text-lg font-semibold italic cursor-pointer after:content-[''] after:absolute after:left-0 after:-bottom-1 after:h-[2px] after:w-full after:bg-red-500 after:scale-x-0 after:origin-left after:transition-transform after:duration-300 hover:after:scale-x-100 hover:text-red-500"
+      >
+        Login
+      </Link>
+    );
   }
 
-  return session ? (
-    <DropdownProfile profile={profile} />
-  ) : (
-    <Link
-      href="/login"
-      className="relative text-lg font-semibold italic cursor-pointer after:content-[''] after:absolute after:left-0 after:-bottom-1 after:h-[2px] after:w-full after:bg-red-500 after:scale-x-0 after:origin-left after:transition-transform after:duration-300 hover:after:scale-x-100 hover:text-red-500"
-    >
-      Login
-    </Link>
-  );
+  return <DropdownProfile profile={profile} />;
 }
